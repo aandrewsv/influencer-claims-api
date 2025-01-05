7d79ab0f4000fa4198511bd16f48cfe0786d0514
aandrewsv <agustin.andrews@gmail.com>
2025-01-05 19:34:48 -0300

    handle cors add with .env variable

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // This adds /api prefix to all routes
 
+  // Configure CORS
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(3000);
 
   // Log all registered routes
